ff2c18e069f56fea1cdd16e4c639b905cd6e3e00
Muhammad Muhajir <muhajirframe@gmail.com>
2018-12-10 06:17:53 +0700

    Update port

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const hello = require("./hello");
 const handleListen = require("./handleListen");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.get("/", hello);
 app.listen(PORT, handleListen(console.log, PORT));
